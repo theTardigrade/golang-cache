b3956500a59b6d75c5f825b97babaecefc7af8f3
James <jismith1989@googlemail.com>
2019-07-08 19:24:55 +0100

    Create Decrement method

--- methods_modify.go
+++ methods_modify.go
@@ -36,3 +36,19 @@ func (c *Cache) Increment(key string) {
 
 	c.data[key] = newCacheDatum(key, count+1)
 }
+
+func (c *Cache) Decrement(key string) {
+	defer c.mutex.Unlock()
+	c.mutex.Lock()
+
+	var count int64
+
+	if datum, exists := c.data[key]; exists {
+		countInterface := datum.value
+		if countValue, ok := countInterface.(int64); ok {
+			count = countValue
+		}
+	}
+
+	c.data[key] = newCacheDatum(key, count-1)
+}
