50caf0855dd390b22b66789f189c7e8ee8c21253
James <jismith1989@googlemail.com>
2019-07-09 17:10:03 +0100

    Remove ErrZeroMaxValues variable

--- error.go
+++ error.go
@@ -3,6 +3,5 @@ package cache
 import "errors"
 
 var (
-	ErrNotFound      = errors.New("cache item not found")
-	ErrZeroMaxValues = errors.New("max values cannot be zero")
+	ErrNotFound = errors.New("cache item not found")
 )
